82397cfcd7bc8adbe50452821a405b1e9eb18136
Techno75 <gdepretz@gmail.com>
2020-11-13 14:42:38 +0100

    cartography opérationnel

--- python-data-ipssi-app/src/views/question1.jsx
+++ python-data-ipssi-app/src/views/question1.jsx
@@ -1,7 +1,27 @@
 import React from 'react';
 import img from './../assets/img/bdd.jpg';
+import url from './../constantes/url';
 
 class Question1 extends React.Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      txt : ""
+    }
+  }
+
+  componentDidMount(){
+    fetch(url + "question-1")
+    .then(res => res.json())
+    .then(data => {
+      this.setState({
+        txt : data.result
+      })
+    })
+  }
+
+
   render(){
     const txt = "CREATE DATABASE IF NOT EXISTS `airport-analytics` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;\n\nUSE `airport-analytics`;\n\nCREATE TABLE airlines\n(\ncarrier VARCHAR(2) PRIMARY KEY,\n`name` VARCHAR(50)\n);\n\nCREATE TABLE planes\n(\ntailnum VARCHAR(6) PRIMARY KEY,\n`type` VARCHAR(30),\n`year` SMALLINT,\nmanufacturer VARCHAR(50),\nmodel VARCHAR(30),\nengines TINYINT,\nseats SMALLINT,\nspeed SMALLINT,\nengine VARCHAR(30)\n);\n\nCREATE TABLE airports\n(\nfaa VARCHAR(3) PRIMARY KEY,\n`name` VARCHAR(100),\nlat FLOAT,\nlon FLOAT,\nalt SMALLINT,\ntz SMALLINT,\ndst CHAR,\ntzone VARCHAR(30)\n);\n\nCREATE TABLE weather\n(\norigin VARCHAR(3),\n`year` SMALLINT,\n`month` TINYINT,\n`day` TINYINT,\n`hour` TINYINT,\ntemp FLOAT,\ndewp FLOAT,\nhumid FLOAT,\nwind_dir SMALLINT,\nwind_speed FLOAT,\nwind_gust FLOAT,\nprecip FLOAT,\npressure FLOAT,\nvisib FLOAT,\ntime_hour DATETIME,\nPRIMARY KEY (`year`, `month`, `day`, hour, origin),\nFOREIGN KEY origin_fk(origin) REFERENCES airports(faa)\n);\n\nCREATE INDEX `month_index` ON weather(`month`);\nCREATE INDEX `day_index` ON weather(`day`);\nCREATE INDEX `hour_index` ON weather(`hour`);\nCREATE INDEX `origin_index` ON weather(`origin`);\n\nCREATE TABLE flights\n(\n`year` SMALLINT,\n`month` TINYINT,\n`day` TINYINT,\ndep_time SMALLINT,\nsched_dep_time SMALLINT,\ndep_delay SMALLINT,\narr_time SMALLINT,\nsched_arr_time SMALLINT,\narr_delay SMALLINT,\ncarrier VARCHAR(2),\nflight SMALLINT,\ntailnum VARCHAR(6),\norigin VARCHAR(3),\ndest VARCHAR(3),\nair_time SMALLINT,\ndistance SMALLINT,\nhour TINYINT,\nminute TINYINT,\ntime_hour DATETIME,\nFOREIGN KEY tailnum_fk(tailnum) REFERENCES planes(tailnum),\nFOREIGN KEY carrier_fk(carrier) REFERENCES airlines(carrier),\nFOREIGN KEY dest_fk(dest) REFERENCES airports(faa),\nFOREIGN KEY origin_airports_fk(origin) REFERENCES airports(faa),\nFOREIGN KEY year_fk(`year`) REFERENCES weather(`year`),\nFOREIGN KEY month_fk(`month`) REFERENCES weather(`month`),\nFOREIGN KEY day_fk(`day`) REFERENCES weather(`day`),\nFOREIGN KEY hour_fk(hour) REFERENCES weather(hour),\nFOREIGN KEY origin_weather_fk(origin) REFERENCES weather(origin)\n);\n\nCREATE TABLE airline_airport\n(\nid INT AUTO_INCREMENT,\nairline_carrier VARCHAR(2) NOT NULL,\nairport_faa VARCHAR(3) NOT NULL,\nPRIMARY KEY (id, airline_carrier, airport_faa),\n    CONSTRAINT `constr_airline_airport_airline_fk`\n    FOREIGN KEY `airline_fk` (airline_carrier) REFERENCES airlines (carrier)\n     ON DELETE CASCADE ON UPDATE CASCADE,\nCONSTRAINT `constr_airline_airport_airport_fk`\n    FOREIGN KEY `airport_fk` (airport_faa) REFERENCES airports (faa)\n     ON DELETE CASCADE ON UPDATE CASCADE\n);"
     
@@ -11,7 +31,7 @@ class Question1 extends React.Component {
           <img src={img} />
           <h1>Les requêtes </h1>
           <pre>
-            {txt}
+            {this.state.txt}
           </pre>
       </main>
     );
--- python-data-ipssi-app/src/views/question8.jsx
+++ python-data-ipssi-app/src/views/question8.jsx
@@ -1,15 +1,116 @@
 import React from 'react';
 import { Map, TileLayer, Marker, Tooltip } from "react-leaflet";
 import AntPath from "react-leaflet-ant-path";
+import url from './../constantes/url';
+import {colorList} from "./../constantes/colorList.js";
 
 class Question8 extends React.Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+     markerList : [],
+     routeList : []
+    }
+  }
+
+  componentDidMount(){
+    fetch(url + "origin_to_dest_gps_data")
+    .then(res => res.json())
+    .then(data => {
+      const lga = data.result.filter(travel => travel.origin === "LGA");
+      const jfk = data.result.filter(travel => travel.origin === "JFK");
+      const ewr = data.result.filter(travel => travel.origin === "EWR");
+
+      const coordList = data.result.map(city => {
+        return {name : city.dest_airport_name, coord : [city.dest_airport_lat , city.dest_airport_lon ]}
+      });
+      coordList.push({name : lga[0].origin_airport_name, coord :[lga[0].origin_airport_lat, lga[0].origin_airport_lon]});
+      coordList.push({name : jfk[0].origin_airport_name, coord :[jfk[0].origin_airport_lat, jfk[0].origin_airport_lon]});
+      coordList.push({name : ewr[0].origin_airport_name, coord :[ewr[0].origin_airport_lat, ewr[0].origin_airport_lon]});
+
+      this.setState({
+        markerList : coordList,
+        jfk,
+        ewr,
+        lga,
+      })
+    })
+  }
+
+
+  createAntPathConfig = (color) => {
+    return (
+      {
+        "delay": 1000,
+        "dashArray": [
+          10,
+          20
+        ],
+        "weight": 6,
+        "color": color,
+        "pulseColor": "#FFFFFF",
+        "paused": false,
+        "reverse": false,
+        "hardwareAccelerated": true
+      }
+    )
+  }
+  
+  diplayRoute = (e) => {
+    if(e.target.value === "John F Kennedy"){
+      const routeListUJfk = this.state.jfk.map((routeTmp => {
+        return [[routeTmp.origin_airport_lat, routeTmp.origin_airport_lon],[routeTmp.dest_airport_lat, routeTmp.dest_airport_lon]]
+      }))
+      this.setState({routeList : [routeListUJfk]})
+    }
+    if(e.target.value === "La Guardia"){
+      const routeListUJfk = this.state.lga.map((routeTmp => {
+        return [[routeTmp.origin_airport_lat, routeTmp.origin_airport_lon],[routeTmp.dest_airport_lat, routeTmp.dest_airport_lon]]
+      }))
+      this.setState({routeList : [routeListUJfk]})
+    }
+    if(e.target.value === "Newark"){
+      const routeListUJfk = this.state.ewr.map((routeTmp => {
+        return [[routeTmp.origin_airport_lat, routeTmp.origin_airport_lon],[routeTmp.dest_airport_lat, routeTmp.dest_airport_lon]]
+      }))
+      this.setState({routeList : [routeListUJfk]})
+    }
+    if(e.target.value === "aucun"){
+      this.setState({routeList : []})
+    }
+  }
+
   render(){
-    const geolocationLat = 25;
-    const geolocationLong = 5;
+    const geolocationLat = 41;
+    const geolocationLong = -97;
     return (
       <main>
-        <Map  bounds={[[geolocationLat-0.01, geolocationLong-0.01],[geolocationLat+0.01, geolocationLong+0.01]]}>
+        <div>
+        <label>Afficher les routes aériènnes de </label>
+        <select onChange={e => this.diplayRoute(e)}>
+          <option>aucun</option>
+          <option>John F Kennedy</option>
+          <option>La Guardia</option>
+          <option>Newark</option>
+        </select>
+        </div>
+        <Map  bounds={[[geolocationLat-20, geolocationLong-20],[geolocationLat+20, geolocationLong+20]]}>
           <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
+          {this.state.routeList.map((route, index) => <AntPath positions={route} options={this.createAntPathConfig(colorList[index])} />)}
+          {this.state.markerList.map((airport, index) => {
+            return(
+              <Marker
+               position={airport.coord}
+               key={index}
+              >
+                <Tooltip>{airport.name}</Tooltip>
+              </Marker>
+            )
+           }
+          )}
+
+
         </Map>
       </main>
     );
